eb6b54cc40613b14878816d7164f3b91c093f021
Fuji <fujiji.entire1414@gmail.com>
2025-08-09 09:35:58 +0900

    match文

--- rs-practice/src/control-flow.rs
+++ rs-practice/src/control-flow.rs
@@ -8,3 +8,32 @@ fn main() {
 
     println!("LIFTOFF!")
 }
+
+// match文
+/*
+*
+*#[derive(Debug)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+fn main() {
+    let red: Color = Color::Red;
+    let green: Color = Color::Green;
+    let blue: Color = Color::Blue;
+    println!("Red Color Code: {}", color_to_str(&red));
+    println!("Green Color Code: {}", color_to_str(&green));
+    println!("Blue Color Code: {}", color_to_str(&blue));
+}
+
+fn color_to_str(color: &Color) -> &str {
+    match color {
+        Color::Red => "#FF0000",
+        Color::Green => "#00FF00",
+        Color::Blue => "#0000FF",
+    }
+}
+
+* */
