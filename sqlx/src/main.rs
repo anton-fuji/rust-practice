72f8ba79a2b60f9cafc0d93371732b081c0c8ec8
Fuji <fujiji.entire1414@gmail.com>
2025-08-28 10:59:03 +0900

    how to use sqlx

---
+++ sqlx/src/main.rs
@@ -0,0 +1,32 @@
+use dotenv;
+use sqlx::{sqlite::SqlitePool, types::chrono};
+use std::env;
+
+#[derive(Debug)]
+pub struct User {
+    pub id: i64,
+    pub name: String,
+    pub email: String,
+    pub address: Option<String>,
+    pub created_at: chrono::NaiveDateTime,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    dotenv::dotenv().expect("Failed to read .env file.");
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set!");
+    let pool = SqlitePool::connect(&database_url).await?;
+    let users = sqlx::query_as!(
+        User,
+        "select id, name, email, address, created_at from users"
+    )
+    .fetch_all(&pool)
+    .await?;
+
+    for user in users {
+        println!("{:?}", user);
+    }
+
+    Ok(())
+}
